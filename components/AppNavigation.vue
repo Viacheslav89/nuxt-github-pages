10cb7cce98d3f419fe180efc1112eb46ba3bd025
To Do List <2113338@mail.ru>
2024-10-26 14:21:27 +0300

    test 2

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -19,9 +19,6 @@
   
   <script setup lang="ts">
     const { isHorizontalList } = useMovies();
-  
-    // const isHorizontalList = ref(false)
-  
     const buttonTitle = computed(() => isHorizontalList.value ? "Блок" : "Список");
   
     const changeDisplayElements = () => {
